6b3135985eea15fe59641e1a1f3e28f94c45e3ac
冯逸洲 <8693548+feng-yizhou@user.noreply.gitee.com>
2023-03-25 14:08:30 +0800

    ok

--- DataStructure/SingleLinkList.py
+++ DataStructure/SingleLinkList.py
@@ -1,13 +1,16 @@
 class SingleNode(object):
-    def __init__(self,item):
+    def __init__(self, item):
         self.item = item
         self.next = None
 
+
 """单链表数据结构类，具有以下功能"""
+
+
 class SingleLinkList(object):
     def __init__(self):
         """存放一个头节点head，好用来指向第一个节点"""
-        self._head = None # _ 私有属性,head=None说明P指向的单链表中没有node
+        self._head = None  # _ 私有属性,head=None说明P指向的单链表中没有node
 
     def isEmpty(self):
         """判断链表是否为空"""
@@ -27,16 +30,16 @@ class SingleLinkList(object):
         """遍历链表"""
         cur = self._head
         while cur != None:
-            print(cur.item,end=" ")
+            print(cur.item, end=" ")
             cur = cur.next
 
-    def add(self,item):
+    def add(self, item):
         """头部添加元素"""
         node = SingleNode(item)
         node.next = self._head
         self._head = node
 
-    def append(self,item):
+    def append(self, item):
         """尾部添加元素"""
         node = SingleNode(item)
         if self.isEmpty():
@@ -46,7 +49,6 @@ class SingleLinkList(object):
             while cur.next != None:
                 cur = cur.next
             cur.next = node
-
 
 
 if __name__ == '__main__':
